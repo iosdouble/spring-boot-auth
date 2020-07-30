a4d9bacaf81c5b968cd11a1755f7bf71401fd0ba
nihui <18202504057@163.com>
2020-07-30 12:12:21 +0800

    init commit

---
+++ src/test/java/com/nh/auth/cloudauth/CloudAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nh.auth.cloudauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
